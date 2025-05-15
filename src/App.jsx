dfb4cabc33eb29477eaaaa2f00f7389690dc65f2
Tanushree Paul <125464289+tanu0604@users.noreply.github.com>
2025-05-15 20:06:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/navigation/Navigation";
 import Home from "./components/home/Home";
+import Courses from "./components/courses/Courses";
 import Service from "./components/services/Service";
 import Testimonials from "./components/testimonials/Testimonials";
 import Footer from "./components/footer/Footer";
 // import Achievement from './components/achievements/Achievement';
-// import Courses from "./components/courses/Courses";
 // import Footer from './components/footer/Footer';
 // import GalleryHome from './components/gallery/GalleryHome';
 // import SchoolGallery from './components/gallery/SchoolGallery';
@@ -24,6 +24,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/services" element={<Service />}></Route>
             <Route path="/testimonials" element={<Testimonials/>}></Route>
+            <Route path="/courses" element={<Courses/>}></Route>
           </Routes>
         </main>
 
