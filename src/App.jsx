263e0c30cffdf1a994d9b036e3ebec291024ef78
Tanushree Paul <125464289+tanu0604@users.noreply.github.com>
2025-05-15 19:53:54 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/navigation/Navigation";
+import Home from "./components/home/Home";
+
+import Footer from "./components/footer/Footer";
+// import Service from './components/services/Service';
+// import Achievement from './components/achievements/Achievement';
+// import Courses from "./components/courses/Courses";
+// import Footer from './components/footer/Footer';
+// import GalleryHome from './components/gallery/GalleryHome';
+// import SchoolGallery from './components/gallery/SchoolGallery';
+// import Testimonials from './components/testimonials/Testimonials';
+
+const App = () => {
+  return (
+    <Router>
+      {/* Entire layout with min-h-screen to fix spacing */}
+      <div className="min-h-screen flex flex-col">
+        <Navigation />
+
+        {/* Main Content Area */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          
+          </Routes>
+        </main>
+
+        {/* Footer stays at the bottom */}
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
