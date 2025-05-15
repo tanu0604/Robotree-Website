e78d6b5ac72d48cc933f47ed4d6daa9c1b7f86c2
Tanushree Paul <125464289+tanu0604@users.noreply.github.com>
2025-05-15 20:24:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/navigation/Navigation";
 import Home from "./components/home/Home";
+import About from "./components/about/About";
+import WhyChooseUs from "./components/about/WhyChooseUs";
+import FutureRobotics from "./components/about/FutureRobotics";
 import Courses from "./components/courses/Courses";
 import Service from "./components/services/Service";
 import Testimonials from "./components/testimonials/Testimonials";
@@ -20,12 +23,18 @@ const App = () => {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/whychooseus" element={<WhyChooseUs />}></Route>
+            <Route path="/future" element={<FutureRobotics />}></Route>
             <Route path="/services" element={<Service />}></Route>
-            <Route path="/testimonials" element={<Testimonials/>}></Route>
-            <Route path="/courses" element={<Courses/>}></Route>
-            <Route path="/achievements" element={<Achievements/>}></Route>
-            <Route path='/gallery' element={<GalleryHome />} />
-            <Route path='/gallery/:schoolId' element={<SchoolGallery/>}></Route>
+            <Route path="/testimonials" element={<Testimonials />}></Route>
+            <Route path="/courses" element={<Courses />}></Route>
+            <Route path="/achievements" element={<Achievements />}></Route>
+            <Route path="/gallery" element={<GalleryHome />} />
+            <Route
+              path="/gallery/:schoolId"
+              element={<SchoolGallery />}
+            ></Route>
           </Routes>
         </main>
 
